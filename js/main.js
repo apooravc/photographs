ce3947855e3c1327ec7af5dfe071fad8e7c42391
Apoorv Choubey <theapoorvs1@gmail.com>
2017-12-14 01:49:52 +0530

    Layout change commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-$(window).on("load", function (){
+$(document).ready(function (){
     $("#gallery").wrapInner("<table cellspacing='20'><tr>");
     $(".post").wrap("<td></td>");
+    $("#gallery").fadeIn(1500);
     $("img").on("contextmenu", function(){
         return false;
     });
