d41e65bffb694b0993320b858f7da72383a4781d
Apoorv Choubey <theapoorvs1@gmail.com>
2017-12-23 23:48:35 +0530

    Mouse plugin add

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function (){
-    $("#gallery").wrapInner("<table cellspacing='20'><tr>");
+    $("#gallery").wrapInner("<table cellspacing='30'><tr>");
     $(".post").wrap("<td></td>");
     $("#gallery").fadeIn(1500);
     $("img").on("contextmenu", function(){
         return false;
+    });
+    $('#gallery').mousewheel(function(e, delta) {
+        this.scrollLeft -= (delta*10);
+        e.preventDefault();
     });
 });
